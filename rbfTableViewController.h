8b0e6feb9ea46deac78d1a22c862ef00f81c3709
matt <Matt@users-mbp.home>
2014-05-14 19:11:07 +0100

    solved the challenge

---
+++ rbfTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  rbfTableViewController.h
+//  UITableViewControllerChallengeSolution
+//
+//  Created by matt on 14/05/2014.
+//  Copyright (c) 2014 matt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rbfTableViewController : UITableViewController
+
+@end
